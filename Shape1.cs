04ac8a7293f728b96d6c672c36046931fdd1f998
Natalia-Dobranova <104311088+Natalia-Dobranova@users.noreply.github.com>
2022-05-07 18:02:37 +0200

    Update Shape1.cs

--- Shape1.cs
+++ Shape1.cs
@@ -1,78 +1,38 @@
-﻿namespace Homework3
+using System;
+
+namespace Homework3
 {
-	public class Shape
-	{
-		public double area;
-		public string name;
-
-		public Shape ()
-		{
-			area = 0;
-			name = "";
-	    }
-
-		public virtual void Area()
-		{
-
-			Console.WriteLine($"The area of {name} is equal to {area} ");
-		}
-	}
-
-	public class Circle : Shape
-	{
-		private double _radius;
-
-		public Circle (double r)
+    public class Shape
+    {
+       public void Area (double radius)
         {
-			_radius = r;
-			name = "circle";
+            double area = Math.PI * Math.Pow(radius, 2);
+
+            Console.WriteLine($"The area of circle is equal to {area} ");
+
+
         }
 
-		public override void Area()
-		{
-			area = Math.PI * Math.Pow(_radius, 2);
-			base.Area();
-		}
-	}
+        public void Area (double a, double h)
+        {
+            double area = 0.5 * a * h;
 
-	public class Triangle : Shape
-	{
-		private double _base;
-		private double _hieght;
+            Console.WriteLine($"The area of triangle is equal to {area} ");
 
-		public Triangle(double a, double h)
-		{
-			_base = a;
-			_hieght = h;
-			name = "triangle";
-		}
 
-		public override void Area()
-		{
-			area = 0.5*_base*_hieght;
-			base.Area();
-		}
-	}
+        }
 
-	public class Trapezoid : Shape
-	{
-		private double _side1;
-		private double _side2;
-		private double _hieght;
+        public void Area (double side1, double side2, double h)
 
-		public Trapezoid(double side1, double side2, double h)
-		{
-			_side1 = side1;
-			_side2 = side2;
-			_hieght = h;
-			name = "trapezoid";
-		}
+        {
+            double area = 0.5 * (side1 + side2) * h;
 
-		public override void Area()
-		{
-			area = 0.5 * (_side1+_side2) * _hieght;
-			base.Area();
-		}
-	}
+            Console.WriteLine($"The area of trapezoid is equal to {area} ");
 
+
+        }
+
+    }
+
+    
 }
